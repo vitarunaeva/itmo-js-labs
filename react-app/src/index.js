238bbe615082cc3233aa91865481806a23678a58
Violetta Runaeva <runaevavioletta@gmail.com>
2019-06-14 01:29:02 +0300

    состояние, классы и двустороннее связывание.

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,11 +1,33 @@
-import React from  'react';
+import React, {Component as C} from  'react';
 import  {render as r} from 'react-dom';
 
-const exps = [10, 1, 2, 3, 4, 5];
-const Deg = ({base: b, exp: e}) =>
-    <h1><span>{b}</span><sup>{e}</sup> = { b ** e }</h1>;
+import  Degs from './components/Degs';
 
-const Degs = ({a}) =>
-    <ul>{a.map((v, i) => <li><Deg base="2" exp={v} key={i} /></li>)}</ul>;
-r(<Degs a={exps} />,
+    class App extends  C {
+        constructor () {
+            super();
+          this.state = {
+              exps: 5
+          }
+        }
+
+        click() {
+        this.setState(({exps}) => ({exps: +exps + 1}));
+        }
+
+        change(v) {
+            this.setState(({exps}) => ({exps: v}))
+        }
+        render() {
+            return (<div>
+                <button onClick={this.click.bind(this)}>Увеличить степень</button>
+                <input value={this.state.exps}
+                    onChange={({ target: {value: v}}) => this.change(v)}/>
+                <Degs a={Array.from({length: this.state.exps }, (v ,i) => 10 + i)}/>
+                </div>)
+        }
+    }
+
+
+    r(<App />,
     document.querySelector('.cont'));
